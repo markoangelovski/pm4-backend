9c629a39ec81ddcef3fe2d86cf007cd407e4b20e
Angelovski <angelovski@marko.com>
2024-11-16 20:22:24 +0100

    Tests updated

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module'; // Import the new UsersModule
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule.register({}), UsersModule], // Import UsersModule here
       providers: [AuthService],
     }).compile();
 
