0603ffb61e29f1044898ec7a936c63424ba14788
Angelovski <marko@angelovski.top>
2024-11-25 22:01:57 +0100

    Tasks CRUD added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,73 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+  Query,
+  BadRequestException,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TasksService } from './tasks.service';
+import { validate } from 'class-validator';
 
 @Controller('tasks')
 @UseGuards(JwtAuthGuard)
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  async getTasks(
+    @Request() req,
+    @Query('projectId') projectId?: string,
+    @Query('status') status?: string,
+  ) {
+    return this.tasksService.getTasks(req.user.userId, projectId, status);
+  }
+
+  @Get(':taskId')
+  async getTask(@Request() req, @Param('taskId') taskId: string) {
+    return this.tasksService.getTask(taskId, req.user.userId);
+  }
+
+  @Post()
+  async createTask(@Request() req, @Body() createTaskDto: CreateTaskDto) {
+    try {
+      const errors = await validate(createTaskDto);
+
+      if (errors.length > 0) {
+        throw new BadRequestException(errors);
+      }
+      return this.tasksService.createTask({
+        ...createTaskDto,
+        userId: req.user.userId,
+      });
+    } catch (error) {
+      console.log('error: ', error);
+      throw new BadRequestException(error);
+    }
+  }
+
+  @Patch(':taskId')
+  async updateTask(
+    @Request() req,
+    @Param('taskId') taskId: string,
+    @Body() updateTaskDto: CreateTaskDto,
+  ) {
+    const errors = await validate(updateTaskDto);
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+    return this.tasksService.updateTask(taskId, req.user.userId, updateTaskDto);
+  }
+
+  @Delete(':taskId')
+  async deleteTask(@Request() req, @Param('taskId') taskId: string) {
+    return this.tasksService.deleteTask(taskId, req.user.userId);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DATABASE_CONNECTION } from '../database/database-connection';
+import * as schema from './schema';
+import { and, eq } from 'drizzle-orm';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @Inject(DATABASE_CONNECTION)
+    private readonly database: NodePgDatabase<typeof import('./schema')>,
+  ) {}
+
+  async getTasks(userId: string, projectId?: string, status?: string) {
+    const whereClause = [eq(schema.Task.userId, userId)];
+
+    if (projectId) {
+      whereClause.push(eq(schema.Task.projectId, projectId));
+    }
+    if (status) {
+      whereClause.push(eq(schema.Task.status, status));
+    }
+    return this.database.query.Task.findMany({
+      where: and(...whereClause),
+    });
+  }
+
+  async getTask(taskId: string, userId: string) {
+    return this.database.query.Task.findFirst({
+      where: and(eq(schema.Task.id, taskId), eq(schema.Task.userId, userId)),
+    });
+  }
+
+  async createTask(taskData: typeof schema.Task.$inferInsert) {
+    return this.database.insert(schema.Task).values(taskData).returning();
+  }
+
+  async updateTask(
+    taskId: string,
+    userId: string,
+    updateTaskDto: typeof schema.Task.$inferInsert,
+  ) {
+    return this.database
+      .update(schema.Task)
+      .set(updateTaskDto)
+      .where(and(eq(schema.Task.id, taskId), eq(schema.Task.userId, userId)))
+      .returning();
+  }
+
+  async deleteTask(taskId: string, userId: string) {
+    return this.database
+      .delete(schema.Task)
+      .where(and(eq(schema.Task.id, taskId), eq(schema.Task.userId, userId)))
+      .returning();
+  }
+}
